297075800d532a834ed05bda97342072ea6bc7ef
S0okJu <damekyung98@gmail.com>
2024-10-29 23:38:46 +0900

    [sc1] spring echo app

---
+++ spring_echo/src/test/java/org/d7mekz/web01/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.d7mekz.web01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring_echo/src/test/java/org/d7mekz/web01/controller/EchoControllerTest.java
@@ -0,0 +1,23 @@
+package org.d7mekz.web01.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EchoController.class)
+public class EchoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testEchoEndpoint() throws Exception {
+        mockMvc.perform(get("/echo"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, World!"));
+    }
+}
